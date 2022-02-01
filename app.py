74a11bcfb5e429689d96bc7d3f7525a1d37c005e
Oah0629 <87073506+Oah0629@users.noreply.github.com>
2022-02-01 19:11:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,17 +31,3 @@ def callback():
         return "OK"
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    input_text = event.message.text
-    if input_text == "$疫情":
-        url = 'https://attach.setn.com/data/dynamic/covid19.json'
-        html = requests.get(url).text
-        data = json.loads(html)  # 轉為字典
-        data1 = list(data["data_1"]["mainData"][0].values())
-        data2 = list(data["data_1"]["mainData"][1].values())
-        data3 = list(data["data_1"]["mainData"][2].values())
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f"本土：{data1[1]}"+"\n"+f"境外：{data2[1]}"+"\n"+f"死亡：{data3[1]}"+"\n"+data["data_1"]["bottomRemarks"]))
-
